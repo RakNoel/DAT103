919ece3026a41609c7858c344dfe80a42ae291a0
oskar <Oskar.leir@gmail.com>
2017-09-04 15:29:01 +0200

    added test comment

--- src/Oving1/filkontroll.sh
+++ src/Oving1/filkontroll.sh
@@ -2,19 +2,18 @@
 #interval = $1
 #filname  = $2
 
-if [ ! -e "$2" ] ; then
-
-    
-else
-    forrigeEdit=$(stat --format "%y" "$2")
-fi
+exists=$([ ! -e "$2" ])
+forrigeEdit=$(stat --format "%y" "$2")
 
 while true; do
     sleep "$1"
-    if [ ! -e "$2" ] ; then
+    if [ [ -e "$2"  ] -a [ ! "$exists" ] ] ; then
+        echo "Filen $2 ble opprettet"
+        break
+    elif [ [ ! -e "$2" ] -a [ "$exists" ] ] ; then
         echo "Filen $2 ble slettet"
         break
-    elif [ "$forrigeEdit" != "$(stat --format %y $2)" ] ; then
+    elif [ [ "$exists" ] -a [ "$forrigeEdit" != "$(stat --format %y $2)" ] ] ; then
         echo "Filen $2 ble endret"
         break
     fi
